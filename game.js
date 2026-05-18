ed88bf196f691e057fd13b25f82c6c06c277a745
Kandula Sushmitha <ksushmitha030@gmail.com>
2026-05-18 22:44:15 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,8 @@
+let num = Math.floor(Math.random()*100)+1;
+let gameOver = false;
+
+const bestScoreEle = document.getElementById("bestScore");
+const guessedNumber = document.getElementById("numberEntered");
+const AttemptEle = document.getElementById("attempt");
+const resetbtn = document.getElementById("reset");
+
